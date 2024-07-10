16a8e97053d650d3035790e066553bad7cce553f
Simeon Tran <siatran@ucsc.edu>
2024-07-10 16:00:15 -0700

    Refactor delay code

--- main/src/delay_server/delay_handler.c
+++ main/src/delay_server/delay_handler.c
@@ -13,39 +13,15 @@ void delayRequestHandler(void* aContext,
                          otMessage *aMessage,
                          const otMessageInfo *aMessageInfo)
 {
-  static uint32_t index = 0;
+  defaultRequestHandler(aContext, aMessage, aMessageInfo);
+  return;
+}
 
-  if (index < DELAY_MAX_PACKETS) {
-    PrintDelimiter();
-
-    uint64_t sent = getTimeSent(aMessage);
-    uint64_t received = 0;
-    otNetworkTimeStatus status = otNetworkTimeGet(OT_INSTANCE, &received);
-
-    if (status == OT_NETWORK_TIME_SYNCHRONIZED) {
-      DelaysUs[index] = received - sent;
-      PrintDelayResults(index, DelaysUs[index]);
-    }
-    else {
-      otLogCritPlat("Current delay test failed due to Time Sync Error.");
-    }
-
-    defaultRequestHandler(aContext, aMessage, aMessageInfo);
-    index += 1;
-
-    PrintDelimiter();
-
-    if (index == DELAY_MAX_PACKETS) {
-      /**
-       * TODO:
-       *  Print out the average delay. You can average it the traditional way, by summing
-       *  everything first, but if you do that, you need to have a way to watch for overflow.
-       *  Another approach is to use an iterative average.
-       */
-      uint64_t averageDelayUs = average(DelaysUs, DELAY_MAX_PACKETS);
-      PrintAverageDelay(averageDelayUs);
-    }
-  }
-
+void defaultRequestHandler(void* aContext,
+                           otMessage *aMessage,
+                           const otMessageInfo *aMessageInfo)
+{
+  printRequest(aMessage, aMessageInfo);
+  sendCoapResponse(aMessage, aMessageInfo);
   return;
 }
