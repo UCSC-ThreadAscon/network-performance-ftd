dbfc86f8ed0583524dd0c7e8ada1a745af14c8ad
Simeon Tran <siatran@ucsc.edu>
2024-07-11 13:47:41 -0700

    Error directive to ensure that throughput and packet lsos experiments run without time sync

--- main/src/main.c
+++ main/src/main.c
@@ -3,7 +3,9 @@
 #include "independent_variables.h"
 
 #if ((DELAY_SERVER || DELAY_CLIENT) && !CONFIG_OPENTHREAD_TIME_SYNC)
-#error "You must turn on Network Time Synchronization for the Delay experiments."
+#error "You must TURN ON Time Sync for the Delay experiments."
+#elif (!(DELAY_SERVER || DELAY_CLIENT) && CONFIG_OPENTHREAD_TIME_SYNC)
+#error "You must TURN OFF Time Sync for the Throughput & Packet Loss experiments."
 #endif
 
 void app_main(void)
