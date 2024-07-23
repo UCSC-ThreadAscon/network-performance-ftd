4779f699e1b159fecab58811766d604c636c7f08
Simeon Tran <siatran@ucsc.edu>
2024-07-23 16:42:53 -0700

    Making good progress on the function

--- main/src/trials.c
+++ main/src/trials.c
@@ -1,34 +1,36 @@
 #include "main.h"
 
-#define MAX_TRIALS 100
+#define NVS_NAMESPACE "nvs_trials"
+#define NVS_TRIALS "trials"
+#define NUM_TRIALS 100
 
 /**
- * Records the number of trials done so far in the Delay experiment,
- * and restarts the device after each trial. After NUM_TRIALS trials have
- * been done, the device stops restarting.
+ * This function should run AFTER each experimental trial is complete. The function
+ * records the numbers of trials done so far, and restarts the device.
  *
  * I discovered the existence of the `esp_reset_reason()` function when reading:
  * https://forum.arduino.cc/t/how-to-distinguish-reset-by-power-on-from-reset-by-software-command/1160400/9
  */
 void startNextTrial()
 {
-  switch (esp_reset_reason())
-  {
-    case ESP_RST_POWERON:
-      break;
+  uint32_t numTrials = 0;
+  nvs_handle_t handle = 0;
+  esp_reset_reason_t reason = esp_reset_reason();
 
-    case ESP_RST_SW:
-      esp_restart();
-      break;
-    
-    default:
-      /**
-       * This branch should ideally NEVER execute. If it has, a problem that
-       * occured during the experiment that resulted in an abnormal reset.
-       */
-      otLogCritPlat("An abnormal reset has occured during this experimental trial.");
-      otLogCritPlat("The experimental trials will need to be rerun.");
+  ESP_ERROR_CHECK(nvs_open(NVS_NAMESPACE, NVS_READWRITE, &handle));
+
+  if (reason == ESP_RST_SW)
+  {
+    ESP_ERROR_CHECK(nvs_get_u32(handle, NVS_TRIALS, &numTrials));
   }
 
+  /** Add in the trial that has just been completed, and write the
+   *  updated number of trials to local storage.
+   */
+  numTrials += 1;
+  ESP_ERROR_CHECK(nvs_set_u32(handle, NVS_TRIALS, &numTrials));
+
+  nvs_close(handle);
+  esp_restart();
   return;
 }
