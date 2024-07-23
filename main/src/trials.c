474cd35d900a2eadc1e356bd68ff018d718e2e48
Simeon Tran <siatran@ucsc.edu>
2024-07-23 16:22:05 -0700

    Defining what to do on certain resets.

--- main/src/trials.c
+++ main/src/trials.c
@@ -1,5 +1,7 @@
 #include "main.h"
 
+#define MAX_TRIALS 100
+
 /**
  * Records the number of trials done so far in the Delay experiment,
  * and restarts the device after each trial. After NUM_TRIALS trials have
@@ -7,6 +9,23 @@
  */
 void startNextTrial()
 {
-  // esp_restart();
+  switch (esp_reset_reason())
+  {
+    case ESP_RST_POWERON:
+      break;
+
+    case ESP_RST_SW:
+      esp_restart();
+      break;
+    
+    default:
+      /**
+       * This branch should ideally NEVER execute. If it has, a problem that
+       * occured during the experiment that resulted in an abnormal reset.
+       */
+      otLogCritPlat("An abnormal reset has occured during this experimental trial.");
+      otLogCritPlat("The experimental trials will need to be rerun.");
+  }
+
   return;
 }
