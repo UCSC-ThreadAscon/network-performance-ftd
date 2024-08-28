e1916ea024dba932b21de2ad6711e321886092ef
Simeon Tran <4740112+SimeonAT@users.noreply.github.com>
2024-08-28 13:21:27 -0700

    Better citation

--- main/include/main.h
+++ main/include/main.h
@@ -36,7 +36,7 @@
 /**
  * I got the idea to use this specific print statement delimiter
  * from the ESP Thread Border Router SDK source code:
- * https://github.com/espressif/esp-thread-br/blob/main/components/esp_ot_br_server/src/esp_br_web.c#L1166
+ * https://github.com/espressif/esp-thread-br/blob/main/components/esp_ot_br_server/src/esp_br_web.c#L1168
  */
 #define PrintDelimiter() otLogNotePlat("<===========================================================>");
 #define PrintCritDelimiter() otLogCritPlat("<===========================================================>");
