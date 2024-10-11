594c192b5219a45a62f90dbe5e1e09a16f157341
Simeon Tran <4740112+SimeonAT@users.noreply.github.com>
2024-10-11 13:46:24 -0700

    Throughput code

--- main/src/throughput.c
+++ main/src/throughput.c
@@ -1,9 +1,22 @@
 #include "tight_loop.h"
+#include "time_api.h"
 #include "main.h"
 
 static otSockAddr socket;
 
-static uint32_t packetsSent = 0;
+static uint32_t packetNum;
+static uint32_t totalBytes;
+static struct timeval startTime;
+static struct timeval endTime;
+
+static inline void resetStaticVariables()
+{
+  packetNum = 0;
+  totalBytes = 0;
+  EmptyMemory(&startTime, sizeof(struct timeval));
+  EmptyMemory(&endTime, sizeof(struct timeval));
+  return;
+}
 
 void tpConfirmableSend(otSockAddr *socket)
 {
@@ -12,7 +25,7 @@ void tpConfirmableSend(otSockAddr *socket)
   request(socket, (void *) &payload, TIGHT_LOOP_PAYLOAD_BYTES, THROUGHPUT_CONFIRMABLE_URI,
           tpConfirmableResponseCallback, OT_COAP_TYPE_CONFIRMABLE);
 
-  packetsSent += 1;
+  packetNum += 1;
   return;
 }
 
@@ -23,9 +36,52 @@ void tpConfirmableResponseCallback(void *aContext,
 {
   defaultResponseCallback(aContext, aMessage, aMessageInfo, aResult);
 
-  if (packetsSent < MAX_PACKETS) {
-    tpConfirmableSend(&socket); // send a request after getting a response.
+  if (packetNum < MAX_PACKETS)
+  {
+    packetNum += 1;
+    totalBytes += getPayloadLength(aMessage);
+
+    if (packetNum == 1)
+    {
+      otLogNotePlat(
+        "Received the first packet! Starting the Throughput Experimental Trial!"
+      );
+      startTime = getTimevalNow();
+    }
+
+    if (packetNum == MAX_PACKETS)
+    {
+      /** The throughput formula is:
+       *
+       *      MAX_PACKETS * PAYLOAD_SIZE_BYTES
+       *      -----------------------------------------   bytes/time
+       *                    t_end - t_start
+       * 
+       */
+      endTime = getTimevalNow();
+
+      double denominatorUs = timeDiffUs(startTime, endTime);
+      double denominatorMs = US_TO_MS(denominatorUs);
+      double denominatorSecs = US_TO_SECONDS(denominatorUs);
+
+      double throughputSecs = totalBytes / denominatorSecs;
+      double throughputMs = totalBytes / denominatorMs;
+      double throughputUs = totalBytes / denominatorUs;
+
+      PrintDelimiter();
+      otLogNotePlat("The throughput is:");
+      otLogNotePlat("%.7f bytes/second, or", throughputSecs);
+      otLogNotePlat("%.7f bytes/ms, or", throughputMs);
+      otLogNotePlat("%.7f bytes/us.", throughputUs);
+      otLogNotePlat("Duration: %.7f seconds", denominatorSecs);
+      otLogNotePlat("Total Received: %" PRIu32 " bytes", totalBytes);
+      PrintDelimiter();
+
+      esp_restart();
+    }
   }
+
+  tpConfirmableSend(&socket);
   return;
 }
 
@@ -34,7 +90,7 @@ void tpConfirmableMain()
   coapStart();
   InitSocket(&socket, SERVER_IP);
 
-  packetsSent = 0;
+  resetStaticVariables();
   tpConfirmableSend(&socket);
   return;
 }
