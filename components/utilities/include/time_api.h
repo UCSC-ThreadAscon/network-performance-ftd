be76e85528e591e9b48902b765288fd8d721d3fb
Simeon Tran <4740112+SimeonAT@users.noreply.github.com>
2024-10-19 12:24:11 -0700

    Remove not needed functions.

--- components/utilities/include/time_api.h
+++ components/utilities/include/time_api.h
@@ -32,6 +32,3 @@ struct timeval getTimevalNow();
 struct timeval getFutureTimeval(uint64_t seconds);
 uint64_t timeDiffMs(struct timeval tv1, struct timeval tv2);
 uint64_t timeDiffUs(struct timeval tv1, struct timeval tv2);
-struct timeval randomTime(struct timeval tv1, struct timeval tv2);
-
-int compareTimevals(const void* ptr1, const void* ptr2);
