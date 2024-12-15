75500f6fddafeaff21422ec4e809608a98df3d66
Simeon Tran <4740112+SimeonAT@users.noreply.github.com>
2024-12-14 20:11:19 -0800

    UDP socket creation function.

--- components/udp_workload/udp_workload.c
+++ components/udp_workload/udp_workload.c
@@ -5,10 +5,16 @@
  * Creates a UDP socket for the client to send packets to the server
  * with the address defined by the macro `CONFIG_SERVER_IP_ADDRESS`.
  */
-void udpCreateSocket(otUdpSocket *socket)
+void udpCreateSocket(otUdpSocket *socket,
+                     otSockAddr *sockAddr)
 {
-  socket->mSockName.mAddress = *otThreadGetMeshLocalEid(OT_INSTANCE);
-  socket->mSockName.mPort = UDP_SOURCE_PORT;
+  sockAddr->mAddress = *otThreadGetMeshLocalEid(OT_INSTANCE);
+  sockAddr->mPort = UDP_SOURCE_PORT;
 
+  handleError(otUdpOpen(OT_INSTANCE, socket, NULL, NULL), "Failed to create UDP socket.");
+
+  char *errorString = sprintf("Failed to bind MLEID and port %d to socket.",
+                              sockAddr->mPort);
+  handleError(otUdpBind(OT_INSTANCE, socket, sockAddr, OT_NETIF_THREAD), errorString);
   return;
 }
