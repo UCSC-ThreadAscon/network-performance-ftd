4fa167d301ba090c0b347578823ff49c720020a0
Simeon Tran <4740112+SimeonAT@users.noreply.github.com>
2024-12-14 22:39:21 -0800

    Opening the UDP socket creates issues where I can't use CLI.

--- components/udp_workload/udp_workload.c
+++ components/udp_workload/udp_workload.c
@@ -8,14 +8,20 @@
 void udpCreateSocket(otUdpSocket *socket,
                      otSockAddr *sockAddr)
 {
-  sockAddr->mAddress = *otThreadGetMeshLocalEid(OT_INSTANCE);
-  sockAddr->mPort = UDP_SOURCE_PORT;
+  if (!otUdpIsOpen(OT_INSTANCE, socket)) {
+    handleError(otUdpOpen(OT_INSTANCE, socket, NULL, NULL), "Failed to create UDP socket.");
 
-  handleError(otUdpOpen(OT_INSTANCE, socket, NULL, NULL), "Failed to create UDP socket.");
-  handleError(otUdpBind(OT_INSTANCE, socket, sockAddr, OT_NETIF_THREAD),
-              "Failed to bind MLEID and UDP port to socket.");
+    // sockAddr->mAddress = *otThreadGetMeshLocalEid(OT_INSTANCE);
+    // sockAddr->mPort = UDP_SOURCE_PORT;
+    // handleError(otUdpBind(OT_INSTANCE, socket, sockAddr, OT_NETIF_THREAD),
+    //             "Failed to bind MLEID and UDP port to socket.");
 
-  otLogNotePlat("Created UDP Socket at port %d.", sockAddr->mPort);
+    // otLogNotePlat("Created UDP Socket at port %d.", sockAddr->mPort);    
+  }
+  else {
+    otLogNotePlat("UDP Socket is already open.");
+  }
+
   return;
 }
 
