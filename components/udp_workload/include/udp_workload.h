5b107f4efe687737f305a6c95d150a912f410432
Simeon Tran <4740112+SimeonAT@users.noreply.github.com>
2025-01-26 13:44:47 -0800

    Doing Throughput Observe experiments

--- components/udp_workload/include/udp_workload.h
+++
@@ -1,9 +0,0 @@
-#include <openthread/udp.h>
-#include <openthread/logging.h>
-#include <openthread/thread.h>
-
-#define UDP_SOURCE_PORT 12345
-#define UDP_DEST_PORT 12345
-
-void udpCreateSocket(otUdpSocket *socket, otSockAddr *sockAddr);
-void udpSend(otUdpSocket *socket, void* payload, uint16_t payloadLength);
